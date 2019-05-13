87b883a354ce9d53cd615015aa4300be3410e727
Hans Gerwitz <hans@gerwitz.com>
2019-05-13 16:51:39 +0200

    Remove deprecated code

--- src/site/search/lunr.json.11ty.js
+++ src/site/search/lunr.json.11ty.js
@@ -9,18 +9,6 @@ class LunrIndex {
     };
   }
 
-  oldrender(data) {
-    var pages = data.collections.writing.map(function(page) {
-      return {
-        url: page.url,
-        title: page.data.title,
-        date: page.date,
-        content: page.templateContent
-      };
-    });
-    return JSON.stringify(pages);
-  }
-
   render(data) {
     var pages = data.collections.writing;
 
