2ceedc93fcd904bd0f7d1cf71ca6c63ae32d340f
gerwitz <hans@gerwitz.com>
2019-09-03 23:14:11 +0200

    reorganization

---
+++ gulp-tasks/generate.js
@@ -0,0 +1,9 @@
+const gulp    = require('gulp');
+const shell   = require('gulp-shell');
+
+/*
+ Run our static site generator to build the pages
+*/
+gulp.task('generate',
+    shell.task('npx eleventy --quiet')
+);
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const gulp  = require('gulp');
-const shell = require('gulp-shell');
-
 
 /**
   Our gulp tasks live in their own files,
@@ -8,34 +6,8 @@ const shell = require('gulp-shell');
  */
 require('require-dir')('./gulp-tasks');
 
-
-/*
- Run our static site generator to build the pages
-*/
-gulp.task('generate', shell.task('DEBUG=*Error* npx eleventy'));
-
-
-/*
-  compile the assets to the correct destination
-*/
-gulp.task('assets', gulp.parallel(
-  'styles',
-  'meta'
-));
-
-/*
-  Let's get the data we need and then build.
-*/
 gulp.task('build', gulp.series(
-  // 'get:data',
-  'assets', // this goes first because it writes to site/_includes
   'generate'
 ));
 
-gulp.task('dev', gulp.parallel(
-  'build',
-  'serve',
-  'watch'
-));
-
-gulp.task('default', gulp.series('dev'));
+gulp.task('default', gulp.series('build'));
