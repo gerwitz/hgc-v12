13ec1b73de2954c15b0b215bf8b63a0d671aff62
gerwitz <hans@gerwitz.com>
2019-09-10 22:27:32 +0200

    Disable week links until destinations are ready

--- filters/weeklink.js
+++ filters/weeklink.js
@@ -4,5 +4,6 @@ const genesis = moment([1974, 2, 9]).startOf('isoWeek');
 module.exports = function(date) {
   thisweek = moment(date).startOf('isoWeek');
   weeknum = thisweek.diff(genesis, 'weeks');
-  return '<a href="/weeks/'+weeknum+'.html">'+weeknum+'</a>';
+  // return '<a href="/weeks/'+weeknum+'.html">'+weeknum+'</a>';
+  return weeknum;
 }
