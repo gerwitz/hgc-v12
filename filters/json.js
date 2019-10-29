f9829ec57f0b5e64536ff8c20fe6d0ce0ee0b153
gerwitz <hans@gerwitz.com>
2019-10-29 19:07:18 +0100

    Prettier debugging

--- filters/json.js
+++ filters/json.js
@@ -1,3 +1,5 @@
-module.exports = function(string, limit) {
-  return JSON.stringify(string);
+const stringify = require('javascript-stringify').stringify;
+
+module.exports = function(string, limit = 3) {
+  return stringify(string, null, "\t", { maxDepth: limit });
 }
