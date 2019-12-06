174169274bcaf043189bbef3f2d3811d1c1f53df
gerwitz <hans@gerwitz.com>
2019-12-06 23:30:58 +0100

    gulp task to check internal links

---
+++ gulp-tasks/links-local.js
@@ -0,0 +1,9 @@
+const gulp    = require('gulp');
+const shell   = require('gulp-shell');
+
+/*
+ test internal links
+*/
+gulp.task('links-internal',
+  shell.task('hyperlink -ri --root _site --canonicalroot https://hans.gerwitz.com/ _site/*.html | tap-spot')
+);
