f5f9540831c3fb5549f5d37f88b944c76663e056
gerwitz <hans@gerwitz.com>
2020-02-29 21:50:49 +0100

    next/prev for weeks

--- collections/weeks.js
+++ collections/weeks.js
@@ -27,6 +27,7 @@ module.exports = function(collection) {
 
   // overwrite current week to hide notes-in-progress
   populatedWeeks.set(current, {
+    current: true,
     fileSlug: current.toString(),
     url: '/weeks/'+current+'/',
     templateContent: '<p>It was still '+moment().format('dddd')+' of this week when the site was last published. Maybe you want <a href="/weeks/'+(current-1)+'/">last week</a>?</p>'
