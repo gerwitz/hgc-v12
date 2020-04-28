2c0de91f3186ef8c2c152b38cfe7181073ac6430
gerwitz <hans@gerwitz.com>
2020-04-28 21:40:01 +0200

    Bootstrapping history events

--- filters/weeklink.js
+++ filters/weeklink.js
@@ -3,7 +3,11 @@ const genesis = moment([1974, 2, 4]); // == moment([1974, 2, 9]).startOf('isoWee
 const NunjucksLib = require("nunjucks");
 
 module.exports = function(date) {
-  thisweek = moment(date).startOf('isoWeek');
-  weeknum = thisweek.diff(genesis, 'weeks');
+  if (date instanceof Date) {
+    thisweek = moment(date).startOf('isoWeek');
+    weeknum = thisweek.diff(genesis, 'weeks');
+  } else { // assume it's a number
+    weeknum = parseInt(date);
+  }
   return new NunjucksLib.runtime.SafeString('<a href="/weeks/'+weeknum+'/">'+weeknum+'</a>');
 }
