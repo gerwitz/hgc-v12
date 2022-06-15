b1fbd1a267208a04d0a56fb5fac70ab33a3a1884
gerwitz <hans@gerwitz.com>
2022-06-15 20:55:12 +0200

    remove gulp

--- gulp-tasks/serve.js
+++
@@ -1,9 +0,0 @@
-const gulp    = require('gulp');
-const shell   = require('gulp-shell');
-
-/*
- local webserver for development
-*/
-gulp.task('serve',
-  shell.task('npx @11ty/eleventy --quiet --serve --port=8008')
-);
