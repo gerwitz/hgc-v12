2a0649effbb4e0652c313abb3179ba3d75bcd038
gerwitz <hans@gerwitz.com>
2022-07-07 18:08:43 +0200

    include the current page in the crumbtrail

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -2,15 +2,15 @@ module.exports = {
   eleventyComputed: {
     crumbtrail: data => {
       if (data.breadcrumbs) {
-        return Array.from(data.breadcrumbs);
+        return Array.from(data.breadcrumbs).concat(data.page.fileSlug);
       }
       else if (data.page.url) {
-        return data.page.url.split('/').filter(path => path).slice(0,-1);
+        return data.page.url.split('/').filter(path => path);
       }
       else {
         // FIXME: this is getting called on pages that also seem to be processed correctly. Are there phantom entities?
         // console.log(data.layout + ' ' + data.page.fileSlug);
-        return ['FIXME'];
+        return ['oops'];
       };
     }
   }
