e44a8b6f6c000b92417772b465148e73cb0e2495
Hans Gerwitz <hans@gerwitz.com>
2025-08-13 16:05:12 +0200

    fix writing breadcrumb date

--- src/writing/writing.11tydata.js
+++ src/writing/writing.11tydata.js
@@ -1,8 +1,15 @@
 export default {
-  // respect draft frontmatter
   eleventyComputed: {
+    // respect draft frontmatter
     eleventyExcludeFromCollections: function(data) {
       return ('draft' in data && data.draft == true);
-    }
+    },
+    // use template date for breadcrumb
+    breadcrumbs: data => [
+      "writing",
+      data.page && data.page.date
+        ? data.page.date.toISOString().slice(0, 10)
+        : undefined
+    ]
   }
 };
